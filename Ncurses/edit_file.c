2daa8a5df452c7f1141a6a9cbe418239d3af4ef5
dcoope33 <dcoope3@g.clemson.edu>
2026-06-12 18:09:21 -0400

    Update edit_file.c

--- Ncurses/edit_file.c
+++ Ncurses/edit_file.c
@@ -1,13 +1,14 @@
 #include <stdio.h>
 
+// this code resets the Highscore to 0 and locks both the unlockable ships.
 int main() {
     FILE *fp = fopen("Highscore", "wb");
-    int score = 538;
+    int score = 0;
     int unlock = 1;
     int lock = 0;
     fwrite(&score, 4, 1, fp);
     fseek(fp, 4, 0);
-    fwrite(&unlock, 4, 1, fp);
+    fwrite(&lock, 4, 1, fp);
     fseek(fp, 4, 4);
     fwrite(&lock, 4, 1, fp);
     fclose(fp);
