c6a7a4518ab5f9cbbcabe4dee29ac56ef71f3ffb
dcoope33 <dcoope3@g.clemson.edu>
2026-06-15 12:34:41 -0400

    Update edit_file.c

--- Ncurses/edit_file.c
+++ Ncurses/edit_file.c
@@ -1,13 +1,15 @@
 #include <stdio.h>
 
+// this code resets the Hiscore file
+
 int main() {
     FILE *fp = fopen("Highscore", "wb");
-    int score = 538;
+    int score = 0;
     int unlock = 1;
     int lock = 0;
     fwrite(&score, 4, 1, fp);
     fseek(fp, 4, 0);
-    fwrite(&unlock, 4, 1, fp);
+    fwrite(&lock, 4, 1, fp);
     fseek(fp, 4, 4);
     fwrite(&lock, 4, 1, fp);
     fclose(fp);
